fd428945510ca5e4fb88a2baf03ac15fd4ec9339
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-29 20:19:19 +0530

    [Ritvik]Add: Welcome Message

---
+++ AddressBook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Program!!!");
+        }
+    }
+}
